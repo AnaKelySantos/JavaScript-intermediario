01b5f2ce374bc75b8cd01166c3a390c82f6c31cd
AnaKelySantos <anasa.prof@gmail.com>
2023-05-26 13:56:39 -0300

    aula2

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//window.onmousemove = function(e){
+  //  if (e.pageY<4 && e.pageX<322) {
+    //    window.alert('pareeee')
+    //}
+//}
+//colocar na agenda de laravel
+
+//localstorage
+
+window.localStorage.setItem("nome", "jõao");
+console.log(localStorage['nome']);
